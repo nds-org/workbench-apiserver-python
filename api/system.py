dbdf20a1cdcee1dac0b9c8f05f5cdade91a1b5ab
Mike Lambert <lambert8@illinois.edu>
2022-02-10 12:42:14 -0600

    Clean-up old code

--- api/system.py
+++
@@ -1,6 +0,0 @@
-import connexion
-
-
-def get_version():
-    print(connexion.context)
-    return "v2.0, bay-beeeee"
--- api/v1/system.py
+++ api/v1/system.py
@@ -3,6 +3,7 @@ import logging
 
 logger = logging.getLogger('api.v1.system')
 
+
 def get_version():
     print(connexion.context)
     return "2.0.0-alpha.1"
