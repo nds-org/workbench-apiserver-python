4e68b1f7228c4a2edb5a271efdd1a6ae3cd09d2d
Sara Lambert <lambert8@illinois.edu>
2022-11-16 15:13:51 -0600

    feat: include latest git commit SHA in GET /api/v1/version

--- api/v1/system.py
+++ api/v1/system.py
@@ -4,22 +4,29 @@ import logging
 from pkg import config
 from yaml import load
 
+import git
+
 logger = logging.getLogger('api.v1.system')
 
 # First fetch to /api/v1/version will cache this
 config.VERSION_NUMBER = None
 
-
 def get_version():
+    if config.VERSION_HASH is None:
+        try:
+            repo = git.Repo(search_parent_directories=True)
+            config.VERSION_HASH = repo.head.object.hexsha
+        except:
+            pass
+
     if config.VERSION_NUMBER is None:
         try:
             with open(config.SWAGGER_URL) as f:
                 yaml_str = f.read()
                 yaml_spec = load(yaml_str)
                 config.VERSION_NUMBER = yaml_spec['info']['version']
-                return config.VERSION_NUMBER, 200
         except:
-            return 'unknown', 404
-    else:
-        return config.VERSION_NUMBER, 200
+            pass
+
+    return {'version': config.VERSION_NUMBER, 'hash': config.VERSION_HASH}, 200
 
