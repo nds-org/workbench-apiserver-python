9a3b7e58d00ada0d7e2ab2a33782dc6b2239fa34
Sara Lambert <lambert8@illinois.edu>
2023-04-22 03:01:34 -0500

    Bugfix/remove git from docker image (#14)
    * feat: consume docker build-args to return metadata
    
    * feat: return additional metadata from /version endpoint

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,11 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
+ARG VERSION=unknown
+ARG BRANCH=unknown
+ARG GITSHA1=unknown
+ARG BUILDNUMBER=unknown
+
 COPY . .
 CMD ["python", "server.py"]
 
--- api/v1/system.py
+++ api/v1/system.py
@@ -1,32 +1,15 @@
-import connexion
 import logging
 
 from pkg import config
-from yaml import load, SafeLoader
-
-import git
 
 logger = logging.getLogger('api.v1.system')
 
-# First fetch to /api/v1/version will cache this
-config.VERSION_NUMBER = None
-
 def get_version():
-    if config.VERSION_HASH is None:
-        try:
-            repo = git.Repo(search_parent_directories=True)
-            config.VERSION_HASH = repo.head.object.hexsha
-        except:
-            pass
-
-    if config.VERSION_NUMBER is None:
-        try:
-            with open(config.SWAGGER_URL) as f:
-                yaml_str = f.read()
-                yaml_spec = load(yaml_str, Loader=SafeLoader)
-                config.VERSION_NUMBER = yaml_spec['info']['version']
-        except:
-            pass
-
-    return {'version': config.VERSION_NUMBER, 'hash': config.VERSION_HASH}, 200
+    return {
+        'name': config.VERSION_NAME,
+        'version': config.VERSION_NUMBER,
+        'hash': config.VERSION_HASH,
+        'branch': config.VERSION_BRANCH,
+        'buildnumber': config.VERSION_BUILDNUMBER,
+    }, 200
 
